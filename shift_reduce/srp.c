300cd38bae002814f552f1850cd3c77b34901c6d
Dona Binu <108735000+donabinu921@users.noreply.github.com>
2024-11-11 16:34:31 +0000

    renamed srp

--- shift_reduce/srp.c
+++ shift_reduce/srp.c
@@ -1,27 +1,27 @@
 #include <stdio.h> 
 #include <string.h> 
 int z = 0, i = 0, j = 0, c = 0; 
-char a[16], ac[20], stk[15], act[10];  
+char input[16], reduce[20], stack[15], shift[10];  
 void check(); 
 int main() 
 { 
  
     puts("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id"); 
     puts("enter input string "); 
-    scanf("%s", a); 
-    c = strlen(a); 
-    strcpy(act, "SHIFT"); 
+    scanf("%s", input); 
+    c = strlen(input); 
+    strcpy(shift, "SHIFT"); 
     puts("stack \t input \t action"); 
     for ( i = 0; j < c; i++, j++) 
       { 
-         printf("\n$%s\t%s$\t%s", stk, a, act); 
-            stk[i] = a[j]; 
-            stk[i + 1] = '\0'; 
-            a[j] = ' '; 
+         printf("\n$%s\t%s$\t%s", stack, input, shift); 
+            stack[i] = input[j]; 
+            stack[i + 1] = '\0'; 
+            input[j] = ' '; 
            check(); 
       } 
-        printf("\n$%s\t%s$\tDONE\n", stk, a); 
-            if(strcmp(stk,"E") == 0) 
+        printf("\n$%s\t%s$\tDONE\n", stack, input); 
+            if(strcmp(stack,"E") == 0) 
   printf("String Accepted\n"); 
             else 
 printf("String Rejected\n"); 
@@ -29,35 +29,35 @@ printf("String Rejected\n");
          
 void check() 
 { 
-    strcpy(ac, "REDUCE by E"); 
+    strcpy(reduce, "REDUCE by E"); 
     for (z = 0; z < c; z++) 
-        if (stk[z] >= 'a' && stk[z] <= 'z') 
-        {printf("\n$%s\t%s$\t%s->id", stk, a, ac); 
-            stk[z] = 'E'; 
-            stk[z + 1] = '\0'; 
+        if (stack[z] >= 'a' && stack[z] <= 'z') 
+        {printf("\n$%s\t%s$\t%s->id", stack, input, reduce); 
+            stack[z] = 'E'; 
+            stack[z + 1] = '\0'; 
  } 
     for (z = 0; z < c; z++) 
-        if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E') 
-        {printf("\n$%s\t%s$\t%s->E+E", stk, a, ac); 
-            stk[z] = 'E'; 
-            stk[z + 1] = '\0'; 
-            stk[z + 2] = '\0'; 
+        if (stack[z] == 'E' && stack[z + 1] == '+' && stack[z + 2] == 'E') 
+        {printf("\n$%s\t%s$\t%s->E+E", stack, input, reduce); 
+            stack[z] = 'E'; 
+            stack[z + 1] = '\0'; 
+            stack[z + 2] = '\0'; 
             i = i - 2; 
         } 
  for (z = 0; z < c; z++) 
-        if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E') 
-        {printf("\n$%s\t%s$\t%s->E*E", stk, a, ac); 
-            stk[z] = 'E'; 
-            stk[z + 1] = '\0'; 
-            stk[z + 2] = '\0'; 
+        if (stack[z] == 'E' && stack[z + 1] == '*' && stack[z + 2] == 'E') 
+        {printf("\n$%s\t%s$\t%s->E*E", stack, input, reduce); 
+            stack[z] = 'E'; 
+            stack[z + 1] = '\0'; 
+            stack[z + 2] = '\0'; 
             i = i - 2; 
         } 
     for (z = 0; z < c; z++) 
-        if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')') 
-        {printf("\n$%s\t%s$\t%s->(E)", stk, a, ac); 
-            stk[z] = 'E'; 
-            stk[z + 1] = '\0'; 
-            stk[z + 2] = '\0'; 
+        if (stack[z] == '(' && stack[z + 1] == 'E' && stack[z + 2] == ')') 
+        {printf("\n$%s\t%s$\t%s->(E)", stack, input, reduce); 
+            stack[z] = 'E'; 
+            stack[z + 1] = '\0'; 
+            stack[z + 2] = '\0'; 
             i = i - 2; 
         } 
 } 
